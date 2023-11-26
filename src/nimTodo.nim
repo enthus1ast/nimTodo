15acbb1f7fae0394ece0e77dbc27bd2984c90e05
David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>
2023-11-26 10:08:21 +0100

    cligen

--- src/nimTodo.nim
+++ src/nimTodo.nim
@@ -1,6 +1,7 @@
-import os, osproc, json, strformat, strutils, tables, std/enumerate
+import os, osproc, json, strformat, strutils, tables, std/enumerate, terminal, cligen
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
+
 
 const basePath = "/home/david/projects/obsidian/diary"
 const matchers = ["DOING", "TODO"]
@@ -11,52 +12,47 @@ type
     lineNumber: int
     line: string
     path: string
+    matcher: string
+
 
 proc `$`(match: Match): string =
   return fmt"{match.path}: {match.line}"
+
 
 iterator find(basePath: string, matchers: openarray[string]): Match =
   for path in walkDirRec(basePath):
     for lineNumber, line in enumerate(path.lines()):
       for matcher in matchers:
         if line.contains(matcher):
-          yield Match(lineNumber: lineNumber, line: line.strip(), path: path)
+          yield Match(lineNumber: lineNumber + 1, line: line.strip(), path: path, matcher: matcher)
 
-var tab: Table[int, Match]
 
-var idx = 1
-for match in find(basePath, matchers):
-  echo fmt"{idx}: {match} :: {idx}"
-  tab[idx] = match
-  idx.inc
+proc main(basePath = basePath) =
+  var tab: Table[int, Match]
 
-# var idx = 1
-# for matcher in matchers:
-#   let rg = fmt"rg '{matcher}' '{basePath}' ---json"
-#   let outp = execCmdEx(rg, {poEvalCommand}).output
-#   for line in outp.splitlines():
-#     ## This is jsonl
-#     if line.isEmptyOrWhitespace(): continue
-#     let js = line.parseJson()
-#     if js["type"].getStr() == "match":
-#       var info: Info
-#       info.path = js["data"]["path"]["text"].getStr()
-#       info.text = js["data"]["lines"]["text"].getStr().strip()
-#       try:
-#         info.lineNumber = js["data"]["line_number"].getInt()
-#       except:
-#         info.lineNumber = 0
-#       tab[idx] = info
-#       echo fmt"{idx}: {info} :: {idx}"
-#       idx.inc
+  var idx = 1
+  for match in find(basePath.absolutePath(), matchers):
+    var style = ""
+    if match.matcher == "DOING":
+      style = ansiForegroundColorCode(fgYellow)
+    else:
+      resetAttributes()
 
-stdout.write("Choose: ")
-var choiceStr = stdin.readLine().strip()
-try:
-  var choiceInt = parseInt(choiceStr)
-  let info = tab[choiceInt]
-  let cmd = fmt"nvim '{info.path}' +:{info.lineNumber}"
-  discard execShellCmd(cmd)
-except:
-  discard
+    echo fmt"{style}{idx}: {match} :: {idx}"
+    tab[idx] = match
+    idx.inc
 
+  resetAttributes()
+  stdout.write("Choose: ")
+  var choiceStr = stdin.readLine().strip()
+  try:
+    var choiceInt = parseInt(choiceStr)
+    let info = tab[choiceInt]
+    let cmd = fmt"nvim '{info.path}' +:{info.lineNumber}"
+    discard execShellCmd(cmd)
+  except:
+    discard
+
+when isMainModule:
+  dispatch(main)
+  
