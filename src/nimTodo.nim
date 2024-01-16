61fc0046406e6cb2ff752060127a8cb8d9daec1b
David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>
2024-01-16 15:10:51 +0100

    tags + tags files

--- src/nimTodo.nim
+++ src/nimTodo.nim
@@ -1,20 +1,23 @@
-import os, strformat, strutils, tables, std/enumerate, terminal, cligen, algorithm, terminal, times
+import os, strformat, strutils, tables, std/enumerate, terminal, cligen, algorithm, terminal, times, sequtils
 import lexer
 
 ## Quick fix list
 # file row col errormessage
 
 const basePath = "/home/david/projects/obsidian/diary"
-const matchers = ["DOING", "TODO", "DONE"]
+const matchers = ["DOING", "TODO", "DONE"] # Matchers for the TODO's
 
 
 type 
+  Path = string
+  Tag = string
   Match = object
     lineNumber: int
     columnNumber: int
     line: string
-    path: string
+    path: Path
     matcher: string
+  Tags = Table[Path, seq[Match]] ## for storing and query tags
 
 proc render(tokens: seq[Token], style: string): string =
   for token in tokens:
@@ -54,6 +57,66 @@ proc toStr(match: Match, style: string, color = true): string =
   else:
     return fmt"{match.path}: {match.line}"
 
+iterator findTags(basePath: string, matchers: openarray[string]): Match =
+  ## Walks through the basePath folder and yields tags messages
+  ## For this it lexes every line it encounters
+  var paths: seq[string] = @[]
+  for path in walkDirRec(basePath):
+    paths.add path
+  paths.sort()
+  for path in paths:
+    for lineNumber, line in enumerate(lines(path)):
+      let tokens = parse(line).filterIt(it.kind == TTag)
+      for token in tokens:
+        if token.data.len() == 1: continue # filter "#" tags; TODO this should be done in parser
+        if token.data[1] == '#': continue # filter "##"... tags; TODO this should be done in parser
+        yield Match(
+          lineNumber: lineNumber + 1,
+          columnNumber: token.col,
+          line: token.data,
+          path: path,
+          matcher: "TAG" # should this be the Content/data of the Token?
+        )
+
+proc populateTags(): Tags =
+  ## Finds all the tags, stores it in tags returns tag table
+  for match in findTags(basePath.absolutePath(), matchers):
+    if not result.contains(match.path):
+      result[match.path] = @[]
+    result[match.path].add match
+
+proc sortedKeys(tags: auto): seq[string] =
+  result = toSeq(tags.keys())
+  result.sort()
+
+proc printPathAndTags(tags: Tags) =
+  ## Print all the tags from the files.
+  ## So: /path/to/file.md #Tag1 #Tag2
+  for path in tags.sortedKeys:
+    let matches = tags[path]
+    echo path, " ", matches.mapIt(it.line).join(" ")
+
+proc normalizeTag(str: string): string =
+  return str.tolower()
+
+proc printTagAndFiles(tags: Tags) =
+  ## Print all the tags from the files.
+  ## So: #Tag1 /path/to/file.md /path/to/file2.md 
+  var tagFile: Table[Tag, seq[Path]]
+  for path in tags.sortedKeys():
+    let matches = tags[path]
+    for match in matches:
+      let tag = match.line.normalizeTag()
+      if not tagFile.contains(tag):
+        tagFile[tag] = @[]
+      tagFile[tag].add path
+  # echo tagFile
+  for tag in tagFile.sortedKeys():
+    let files = tagFile[tag] 
+    echo tag
+    for file in files.sorted():
+      echo "\t" & file
+    echo ""
 
 iterator find(basePath: string, matchers: openarray[string]): Match =
   var paths: seq[string] = @[]
@@ -81,12 +144,26 @@ proc ctrlc() {.noconv.} =
   echo ""
   quit()
 
-proc main(basePath = basePath, absolutePath = false, showDone = false, quiet = false, clist = false, doingOnly = false, newFile = false) =
+proc main(basePath = basePath, absolutePath = false, showDone = false,
+    quiet = false, clist = false, doingOnly = false, newFile = false,
+    tags = false, tagsFiles = false) =
   ## `basePath` is the path which is searched
   ## when `absolutePath` is true print the whole pat
   ## when `json` is true print the output as json, the user is not asked then.
   ## when `quiet` is true, do not ask for the file
   setControlCHook(ctrlc)
+
+  # # Handle "tags" this just prints all the tags
+  if tags:
+    let tagsTable = populateTags()
+    tagsTable.printPathAndTags()
+    quit()
+  
+  # Handle "tags" this just prints all the tags
+  if tagsFiles:
+    let tagsTable = populateTags()
+    tagsTable.printTagAndFiles()
+    quit()
 
   # Handle "newFile" which is special since it directly opens todays file
   if newFile:
@@ -154,7 +231,8 @@ when isMainModule:
     },
     short={
       "absolutePath": 'p',
-      "showDone": 'a'
+      "showDone": 'a',
+      "tagsFiles": 'f'
     }
   )
   
