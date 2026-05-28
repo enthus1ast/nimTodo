26336f8fe0cef3ee052b4dced65e48093106c409
David Krause (enthus1ast) <david.git@dkrause.org>
2026-05-28 12:13:29 +0200

    look for the config.ini in multiple places

--- src/configs.nim
+++ src/configs.nim
@@ -16,8 +16,21 @@ type
     extentionsToOpen*: seq[string]
 
 
-# global config object
-let config* = loadObject[Config](getAppDir() / "config.ini", false)
+proc getConfigPath(): string = 
+  ## looks for a config based on the below search order and returns the path to it
+  # Config search order:
+  let configSearchOrder = @[
+    getAppDir() / "config.ini",     # 1. right next to the executable
+    "~/.config/nimTodo/config.ini".expandTilde(),   # 2. in the user dirs config
+    "/etc/nimTodo/config.ini"        # 3. in the global config
+  ]
+  for possibleConfigPath in configSearchOrder:
+    if possibleConfigPath.fileExists():
+      # echo "# Use config: ", possibleConfigPath
+      return possibleConfigPath
+
+var configPath: string = getConfigPath()
+let config* = loadObject[Config](configPath, false) # global config object
 
 # basic tests if config is valid
 if config.matchers.len != 4:
